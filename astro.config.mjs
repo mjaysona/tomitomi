81bd947527d9205352b63a04e1d68abe3e82ea5e
Mark Jayson Alimboyogen <mark.jayson.alimboyogen@ing.com>
2022-09-18 16:45:56 +0800

    Change markdown images implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
+import remarkAstrojsImage from "@altano/remark-astrojs-image";
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -6,7 +8,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+    image(),
+    mdx({
+      remarkPlugins: [remarkAstrojsImage],
+    }),
+    sitemap(),
+  ],
   server: { port: 3003 },
   vite: {
     ssr: {
